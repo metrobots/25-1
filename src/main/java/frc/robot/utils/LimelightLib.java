300d85af67f88f6a3548c8105f053102d683ec9b
metrobots <flabs.buy@gmail.com>
2025-01-03 13:09:56 -0800

    added a limelight library

--- src/main/java/frc/robot/utils/LimelightLib.java
+++ src/main/java/frc/robot/utils/LimelightLib.java
@@ -6,7 +6,7 @@ import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.TimestampedDoubleArray;
 import frc.robot.utils.LimelightLib.LimelightResults;
-import frc.robot.utils.LimelightLib.PoseEstimate;
+import frc.robot.utils.LimelightLib.PoseEstimate; 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
