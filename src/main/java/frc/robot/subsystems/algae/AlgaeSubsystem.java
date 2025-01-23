bf3b84107672304cbc9d97245eb8fe6f5d1929c5
Blue Hat Aspie <ehmdiagne0530@gmail.com>
2025-01-23 15:44:11 -0500

    update shoot algae (no testing :))

--- src/main/java/frc/robot/subsystems/algae/AlgaeSubsystem.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeSubsystem.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.algae;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.ControlType;
 import com.revrobotics.spark.SparkClosedLoopController;
@@ -17,13 +16,10 @@ public class AlgaeSubsystem extends SubsystemBase {
     private final SparkClosedLoopController bottomMotorController;
     private final SparkClosedLoopController pivotMotorController;
 
-    // Completely randomly chosen number. Will change later.
-    private static final double motorSpeed = 20.0;
-
     public AlgaeSubsystem(int topMotorCanID, int bottomMotorCanID, int pivotCanID,
             double pivotSpeed, double topMotorSpeed, double bottomMotorSpeed) {
 
-        // TODO: check if brushless.
+        // TODO: Check if brushless.
         this.topMotor = new SparkMax(topMotorCanID, MotorType.kBrushless);
         this.bottomMotor = new SparkMax(bottomMotorCanID, MotorType.kBrushless);
         this.pivotMotor = new SparkMax(pivotCanID, MotorType.kBrushless);
@@ -37,15 +33,18 @@ public class AlgaeSubsystem extends SubsystemBase {
         this.pivotMotor.set(pivotSpeed);
     }
 
-    // TODO: name less weird.
-    public void driveOutward() {
-        this.topMotorController.setReference(motorSpeed, ControlType.kVelocity);
-        this.bottomMotorController.setReference(motorSpeed, ControlType.kVelocity);
+    public void driveOutward(double speed) {
+        this.topMotorController.setReference(speed, ControlType.kVelocity);
+        this.bottomMotorController.setReference(-speed, ControlType.kVelocity);
     }
 
-    // TODO: name less weird.
-    public void driveInward() {
-        this.topMotorController.setReference(-motorSpeed, ControlType.kVelocity);
-        this.bottomMotorController.setReference(-motorSpeed, ControlType.kVelocity);
+    public void driveInward(double speed) {
+        this.topMotorController.setReference(-speed, ControlType.kVelocity);
+        this.bottomMotorController.setReference(speed, ControlType.kVelocity);
+    }
+
+    public void stopDrive() {
+        this.topMotorController.setReference(0, ControlType.kVelocity);
+        this.topMotorController.setReference(0, ControlType.kVelocity);
     }
 }
--- src/main/java/frc/robot/subsystems/algae/commands/shootAlgae.java
+++ src/main/java/frc/robot/subsystems/algae/commands/shootAlgae.java
@@ -1,30 +1,32 @@
 package frc.robot.subsystems.algae.commands;
 
-import com.revrobotics.AbsoluteEncoder;
-import com.revrobotics.spark.SparkMax;
-
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.algae.AlgaeSubsystem;
-
-import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 public class ShootAlgae extends Command {
 
     /* Placeholder until real one is added. */
-    private AlgaeSubsystem algae;
+    private AlgaeSubsystem algaeSubsystem;
+    private final double SPEED = 60.0;
+
+    public ShootAlgae(AlgaeSubsystem subsystem) {
+        this.algaeSubsystem = subsystem;
+        addRequirements(this.algaeSubsystem);
+    }
 
     @Override
     public void initialize() {
+        this.algaeSubsystem.stopDrive();
     }
 
     @Override
     public void execute() {
-
+        algaeSubsystem.driveOutward(SPEED);
     }
 
     @Override
     public void end(boolean interrupted) {
-
+        algaeSubsystem.stopDrive();
     }
 
     @Override
