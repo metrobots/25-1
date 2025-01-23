e39a74ab034908ef1614cc38bb4af21c21e148bc
Blue Hat Aspie <ehmdiagne0530@gmail.com>
2025-01-23 17:45:47 -0500

    descriptive comment

--- src/main/java/frc/robot/subsystems/algae/AlgaeSubsystem.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeSubsystem.java
@@ -3,8 +3,13 @@ package frc.robot.subsystems.algae;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
 import com.revrobotics.spark.SparkClosedLoopController;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import frc.robot.utils.Constants.DriveConstants;
 
 public class AlgaeSubsystem extends SubsystemBase {
 
@@ -16,13 +21,19 @@ public class AlgaeSubsystem extends SubsystemBase {
     private final SparkClosedLoopController bottomMotorController;
     private final SparkClosedLoopController pivotMotorController;
 
-    public AlgaeSubsystem(int topMotorCanID, int bottomMotorCanID, int pivotCanID,
-            double pivotSpeed, double topMotorSpeed, double bottomMotorSpeed) {
+    private final SparkMaxConfig motorConfig = new SparkMaxConfig();
+
+    public AlgaeSubsystem(double pivotSpeed, double topMotorSpeed, double bottomMotorSpeed) {
+        motorConfig.inverted(false);
 
         // TODO: Check if brushless.
-        this.topMotor = new SparkMax(topMotorCanID, MotorType.kBrushless);
-        this.bottomMotor = new SparkMax(bottomMotorCanID, MotorType.kBrushless);
-        this.pivotMotor = new SparkMax(pivotCanID, MotorType.kBrushless);
+        this.topMotor = new SparkMax(DriveConstants.topAlgaeCanId, MotorType.kBrushless);
+        this.bottomMotor = new SparkMax(DriveConstants.bottomAlgaeCanId, MotorType.kBrushless);
+        this.pivotMotor = new SparkMax(DriveConstants.algaePivotCanId, MotorType.kBrushless);
+
+        this.topMotor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        this.bottomMotor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+        this.pivotMotor.configure(motorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
 
         this.topMotorController = topMotor.getClosedLoopController();
         this.bottomMotorController = bottomMotor.getClosedLoopController();
