9109cc612304bea3abff8ca9370b568fd897edf8
EHMD28 <ehmdiagne0530@gmail.com>
2025-03-08 11:27:14 -0500

    removed algae commands

--- src/main/java/frc/robot/subsystems/algae/AlgaeSubsystem.java
+++ src/main/java/frc/robot/subsystems/algae/AlgaeSubsystem.java
@@ -1,7 +1,6 @@
 package frc.robot.subsystems.algae;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 import com.revrobotics.RelativeEncoder;
